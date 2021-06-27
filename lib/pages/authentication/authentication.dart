ceb8b29eecf5e2d106414da3b7a0d3a3b09c8628
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-27 17:32:41 +0000

    feat: initial screen

---
+++ lib/pages/authentication/authentication.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_project/widgets/custom_text.dart';
+
+class AuthenticationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomText(
+        text: "Authentication",
+      ),
+    );
+  }
+}
