ed352aa4b54eda506e2f48c90c3b0dd0cc135018
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-27 16:52:01 +0000

    feat: large screen state builder

---
+++ lib/widgets/large_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_project/widgets/side_menu.dart';
+
+class LargeScreen extends StatelessWidget {
+  const LargeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.red,
+            child: SideMenu(),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            color: Colors.blue,
+          ),
+        )
+      ],
+    );
+  }
+}
