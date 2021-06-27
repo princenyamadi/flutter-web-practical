f009983feeeaed72bf2f8636a87e7ced6837498c
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-27 16:47:51 +0000

    feat: controllers

---
+++ lib/constants/controllers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_web_project/controllers/menu_controller.dart';
+import 'package:flutter_web_project/controllers/navigation_controller.dart';
+
+MenuController menuController = MenuController.instance;
+
+NavigationController naviagtionController = NavigationController.instance;
