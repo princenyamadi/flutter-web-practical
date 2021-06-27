3fb39d8d4e2739379eb7ea3d9f24df31eecb6db3
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-27 17:33:14 +0000

    feat: initial screen

---
+++ lib/pages/drivers/drivers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_project/widgets/custom_text.dart';
+
+class DriversPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomText(
+        text: "Drivers",
+      ),
+    );
+  }
+}
