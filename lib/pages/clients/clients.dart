571cd88c11706eac8527ce0fdb849caba037a166
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-27 17:32:55 +0000

    feat: initial screen

---
+++ lib/pages/clients/clients.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_project/widgets/custom_text.dart';
+
+class ClientsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomText(
+        text: "Clients",
+      ),
+    );
+  }
+}
