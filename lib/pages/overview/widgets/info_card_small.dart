73a464c0f8481e5c5c60ab10a6f7b8f0c348b54e
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-28 00:23:15 +0000

    feat: info card display for small screens

---
+++ lib/pages/overview/widgets/info_card_small.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_project/constants/style.dart';
+import 'package:flutter_web_project/widgets/custom_text.dart';
+
+class InfoCardSmall extends StatelessWidget {
+  final String title;
+  final String value;
+  final bool isActive;
+  final Function onTap;
+
+  const InfoCardSmall(
+      {Key key, this.title, this.value, this.isActive = false, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: isActive ? active : lightGrey, width: 5),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomText(
+                text: title,
+                size: 24,
+                weight: FontWeight.w300,
+                color: isActive ? active : lightGrey,
+              ),
+              CustomText(
+                text: title,
+                size: 24,
+                weight: FontWeight.bold,
+                color: isActive ? active : lightGrey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
