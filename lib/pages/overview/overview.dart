cf13254da7a24c4e86e15697abc533234607d67e
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-28 00:28:40 +0000

    feat: general overview screen widget

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_project/constants/controllers.dart';
+import 'package:flutter_web_project/helpers/responsiveness.dart';
+import 'package:flutter_web_project/pages/overview/widgets/overview_card_large.dart';
+import 'package:flutter_web_project/pages/overview/widgets/overview_card_medium.dart';
+import 'package:flutter_web_project/pages/overview/widgets/overview_card_small.dart';
 import 'package:flutter_web_project/widgets/custom_text.dart';
+import 'package:get/get.dart';
 
 class OverViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomText(
-        text: "OverView",
-      ),
+    return Column(
+      children: [
+        Obx(
+          () => Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                    top: ResponsiveWidget.isSmallScreen(context) ? 56 : 6),
+                child: CustomText(
+                  text: menuController.activeItem.value,
+                  size: 24,
+                  weight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              if (ResponsiveWidget.isLargeScreen(context) ||
+                  ResponsiveWidget.isMediumScreen(context))
+                if (ResponsiveWidget.isCustomScreen(context))
+                  OverviewCardsMediumScreenSizes()
+                else
+                  OverviewCardsLargeScreen()
+              else
+                OverviewCardsSmallScreen()
+            ],
+          ),
+        )
+      ],
     );
   }
 }
